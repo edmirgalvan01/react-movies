f42f02b00081818eaeb50dc5e2ae3704814abf5f
Edmir Galvan <edmirgalvaz@gmail.com>
2023-03-07 07:28:22 -0600

    MoviePage

--- src/components/pages/MoviePage/MoviePage.tsx
+++ src/components/pages/MoviePage/MoviePage.tsx
@@ -1,7 +1,17 @@
 import { useParams } from "react-router-dom";
+import { useGetMovieDetails } from "../../../hooks/useGetMovieDetails";
 
 export const MoviePage = () => {
   const { id } = useParams();
+  const { movie } = useGetMovieDetails(id!);
 
-  return <div>MoviePage</div>;
+  const imgSrc = `http://image.tmdb.org/t/p/w500/${movie?.poster_path}`;
+  const imgAlt = `Imagen aluciva a la pelicula ${movie?.title}`;
+
+  return (
+    <section className="moviePage">
+      <h1>{movie?.title}</h1>
+      <img src={imgSrc} alt={imgAlt} />
+    </section>
+  );
 };
