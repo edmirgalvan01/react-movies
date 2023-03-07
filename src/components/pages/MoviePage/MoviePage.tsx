f70b6568c0ab38faa277ea0b23ac84f6b9b9118e
Edmir Galvan <edmirgalvaz@gmail.com>
2023-03-07 07:19:25 -0600

    getMovieDetails

--- src/components/pages/MoviePage/MoviePage.tsx
+++ src/components/pages/MoviePage/MoviePage.tsx
@@ -1,3 +1,7 @@
+import { useParams } from "react-router-dom";
+
 export const MoviePage = () => {
+  const { id } = useParams();
+
   return <div>MoviePage</div>;
 };
--- src/services/movies.service.ts
+++ src/services/movies.service.ts
@@ -1,8 +1,16 @@
+import type { DetailsOfMovieType, ResponseOfMovies } from "../types/Movies";
 import { API_KEY, API_URL } from "../../consts";
-import { ResponseOfMovies } from "../types/Movies";
 
 export const discoverMovies = async (): Promise<ResponseOfMovies> => {
   const data = await fetch(`${API_URL}/discover/movie?api_key=${API_KEY}`);
+  const response = await data.json();
+  return response;
+};
+
+export const getMovieDetails = async (
+  id: number
+): Promise<DetailsOfMovieType> => {
+  const data = await fetch(`${API_URL}/movie/${id}?api_key=${API_KEY}`);
   const response = await data.json();
   return response;
 };
